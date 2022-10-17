54bd47957b9a6ade201091e36ff0c06e6cb0f177
nat-netl <nat_netl3@bk.ru>
2022-10-17 16:08:55 +0300

    form

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
-
-
-const TelegramBot = require('node-telegram-bot-api');
-require('dotenv').config()
+const TelegramBot = require("node-telegram-bot-api");
+require("dotenv").config();
 
 const token = process.env.BOT_TOKEN;
-const webAppUrl = "https://www.ozon.ru/"
-const bot = new TelegramBot(token, {polling: true});
+const webAppUrl = process.env.WEB_APP_URL;
+const bot = new TelegramBot(token, { polling: true });
 
-
-bot.on('message', async (msg) => {
+bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
   const text = msg.text;
 
-  if (text === '/start') {
-    await bot.sendMessage (chatId ,'Ниже появится кнопка, тыкни и появится форма', {
-      reply_markup: {
-        keyboard: [
-          [{text: 'Заполнить кнопку', web_app: {url: webAppUrl}}]
-        ]
+  if (text === "/start") {
+    await bot.sendMessage(
+      chatId,
+      "Ниже появится кнопка, тыкни и появится форма",
+      {
+        reply_markup: {
+          keyboard: [
+            [{ text: "Заполнить форму", web_app: { url: webAppUrl + '/form' } }],
+          ],
+        },
       }
-    })
+    );
 
-    await bot.sendMessage (chatId ,'Заходи в наш интернет магазин, по кнопке внизу', {
-      reply_markup: {
-        inline_keyboard: [
-          [{text: 'Сделать заказ', web_app: {url: webAppUrl}}]
-        ]
+    await bot.sendMessage(
+      chatId,
+      "Заходи в наш интернет магазин, по кнопке внизу",
+      {
+        reply_markup: {
+          inline_keyboard: [
+            [{ text: "Сделать заказ", web_app: { url: webAppUrl } }],
+          ],
+        },
       }
-    })
-  } 
-
+    );
+  }
 });
